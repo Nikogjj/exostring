78baa30846fbb86e91419bfeaf8404ac3879258e
niko <nikolaguemmoud@hotmail.fr>
2024-10-08 09:35:05 +0200

    exo

--- exostring2.c
+++ exostring2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
 
     
@@ -13,18 +13,37 @@ int main(){
         
     for (int i = 0; i < 6; i++)
     {
-        for (int j = 0;  tab[i][j] < 40; j++)
-        {
-            if (tab[i][j]>90)
-            {
-                tab[i][j]=tab[i][j]-32;
+        for (int j = 0;  tab[i][j]!=0; j++)
+        {   
+            
+            // if (tab[i][j]>90)
+            // {
+            //     tab[i][j]=tab[i][j]-32;
                 
-            }
-            else{
-                tab[i][j]=tab[i][j]+32;
+            // }
+            // else{
+            //     tab[i][j]=tab[i][j]+32;
+            // }
+            switch (tab[i][j])
+            {
+                case 32:
+                    tab[i][j]=32;
+                    break;
+                case 63:
+                    tab[i][j]=63;
+                    break;
+                default:
+                        if (tab[i][j]>90)
+                    {
+                        tab[i][j]=tab[i][j]-32;
+                        
+                    }
+                    else{
+                        tab[i][j]=tab[i][j]+32;
+                    }
+                break;
             }
         }
-
             printf("%s-\n",tab[i]);
             
     }
